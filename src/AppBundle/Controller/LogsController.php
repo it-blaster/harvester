1ff46d0385c54db6428ca215f8f0b8dfa17b6d76
0x2f8f <0x2f8f@gmail.com>
2016-05-08 13:50:51 +0500

    logcontroller

---
+++ src/AppBundle/Controller/LogsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @Route("/logs")
+ */
+class LogsController extends Controller
+{
+    /**
+     * @Route("/add", name="log-add")
+     */
+    public function addAction(Request $request)
+    {
+        
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
+        ]);
+    }
+}
